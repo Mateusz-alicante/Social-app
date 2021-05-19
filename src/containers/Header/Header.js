aa9f8a9c5e260f94f87156e60d415dadcdf009dc
Mateusz-alicante <mateusz.alicante@gmail.com>
2021-05-19 14:13:29 +0200

    3 files modified, 4 added

---
+++ src/containers/Header/Header.js
@@ -0,0 +1,12 @@
+import React, { useEffect } from 'react';
+import styles from './Header.module.css';
+
+const Layout = props => {
+  return (
+    <div className={styles.outerContainer}>
+      <h1>Scam Chatter</h1>
+    </div>
+  );
+};
+
+export default Layout;
--- src/containers/layout/layout.js
+++ src/containers/layout/layout.js
@@ -1,15 +1,16 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import styles from './layout.module.css';
 
+import Header from '../Header/Header.js';
+
 const Layout = props => {
-  useEffect(() => {
-    console.log(styles);
-  }, []);
   return (
-    <div style={{ color: 'red' }}>
-      <div>header</div>
-      <div>contacts</div>
-      <div>main screen</div>
+    <div className={styles.outerContainer}>
+      <Header />
+      <div className={styles.Layout}>
+        <div className={styles.contacts}>contacts</div>
+        <div className={styles.chat}>main screen</div>
+      </div>
     </div>
   );
 };
